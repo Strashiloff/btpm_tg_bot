d18936766d60573d7a7b87d2197f325214446a05
Alexander Shilov <a.shilov@promo-bot.ru>
2021-07-19 02:49:38 +0500

    Fix error in output server status

--- utils/parser.py
+++ utils/parser.py
@@ -1,12 +1,15 @@
 def parsePlayers (string):
   arr = string.split(' ')
   players = ''
+  online = 0
   try:
-    online = int(arr[5])
+    online = int(arr[2])
   except Exception as e:
     return 0, 'никого нет('
   if online > 0:
-    for i in range(13, len(arr)):
+    for i in range(10, len(arr)):
       players += arr[i]
+      if i < len(arr) - 1:
+        players += ', '
       
   return online, players
